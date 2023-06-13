1bd44a49778a9297adbdfb1383b9587fdafc73ea
deadly-nightshade <eliyna.carienne@gmail.com>
2023-06-13 20:13:29 +0800

    Update streamlit_app.py
    trial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
 
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+from torchvision import models, transforms
+import torch
+
+# title and logo
+st.title("App Name")
+# st.set_page_config(page_title= “Evoke Ex-Stream App”, page_icon=”evoke_logo.png”)
+
+
+st.write("Upload your video here, or take a video from your webcam!")
