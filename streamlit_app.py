1eaf2b4e991a03ea56537685f6e63397d2f1661b
Simul-Eqn <89591030+Simul-Eqn@users.noreply.github.com>
2023-06-13 21:16:08 +0800

    add structure to app with comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,25 @@ st.title("App Name")
 # st.set_page_config(page_title= “Evoke Ex-Stream App”, page_icon=”evoke_logo.png”)
 
 
+# The following comments have bracket corresponding to the tep numbers in the workflow in the google docs 
+
+# First, do all the preprocessing + defining functions and stuff 
+
+# (2) define function to turn video into image frames, inputs are video (opencv format) and the time between frames (to be extracted) 
+
+# (3) define function to do image captioning on each frame 
+
+
+
+#Now, the main website itself 
+
+# (1) Take a video 
+
 st.write("Upload your video here, or take a video from your webcam!")
+
+
+
+
+
+
+
