97f1ba828efb46388dec7eda7d26ecc6f4e6033e
Simul-Eqn <89591030+Simul-Eqn@users.noreply.github.com>
2023-06-15 11:29:57 +0800

    delete unused variable and clean up ui

--- Upload_Surveillance_Footage.py
+++ Upload_Surveillance_Footage.py
@@ -268,11 +268,6 @@ vectorizer = getVectorizer()
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 values = loadValues() 
 
-#display everything 
-
-video_type = st.sidebar.selectbox("Choose footage input mode", ["Upload footage", "Real-time footage"])
-
-
 #1. Upload Video
 
 def upload_page():
